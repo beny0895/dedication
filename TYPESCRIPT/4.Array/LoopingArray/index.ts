d9a721905e7d545a871b6a16b3b588de19a8a636
Beny0895 <gunadibeny@gmail.com>
2026-03-04 13:11:20 +0700

    feat: Looping-Array

---
+++ TYPESCRIPT/4.Array/LoopingArray/index.ts
@@ -0,0 +1,37 @@
+// WEDNESDAY, 4 MARCH 2026, 12:30 - 13:05
+
+let animals = ["fish", "bird", "cat", "bear"];
+console.log(animals.length);
+
+for(let i = 0; i < animals.length; i++) {
+    console.log(i);
+};
+
+animals.forEach(function(animals, index) {
+    console.log(`${index} : ${animals}`);
+
+    console.log(animals);
+});
+
+interface Istudent {
+    firstname: string,
+    lastname: string,
+    age: number,
+    // [x: string] : string | number
+};
+const student: Istudent= {
+    firstname : "budi",
+    lastname: "taniagus",
+    age: 22,
+};
+// for in
+for (let x in student) {
+    console.log(`${x} : ${student[x as keyof Istudent]}`);
+};
+
+// for
+const keys = Object.keys(student) as (keyof Istudent)[];
+for (let i = 0; i < keys.length; i++) {
+  const key = keys[i];
+  console.log(`${key} : ${student[key as keyof Istudent]}`);
+};
