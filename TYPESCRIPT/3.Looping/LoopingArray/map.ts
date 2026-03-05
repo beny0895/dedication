c7b6096b0223c06edac12e60230215940876d9f6
Beny0895 <gunadibeny@gmail.com>
2026-03-05 23:34:25 +0700

    feat: Looping-Map

--- TYPESCRIPT/3.Looping/LoopingArray/map.ts
+++ TYPESCRIPT/3.Looping/LoopingArray/map.ts
@@ -2,14 +2,14 @@
 
 const name = ["budi", "george", "alexander", "steven"];
 const toUppercaseName = name.map(function (variable, index, array) {
-    return variable.toLocaleUpperCase()
+    return variable.toLocaleUpperCase();
     // console.log(`${index} : ${variable} : ${array}`)
-})
+});
 console.log(toUppercaseName);
 console.log(name);
 
-const number = [1, 3, 4, 5, 16]
+const number = [1, 3, 4, 5, 16];
 const multipleNumber = number.map((number)=> {
-    return number * 2
-})
-console.log(multipleNumber)
+    return number * 2;
+});
+console.log(multipleNumber);
