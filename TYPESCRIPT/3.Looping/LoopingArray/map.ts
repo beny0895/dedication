afa5c5f7022d6eaa4779bae02ef4b73069ab3da2
Beny0895 <gunadibeny@gmail.com>
2026-03-05 23:32:13 +0700

    feat: Looping-ForEach

---
+++ TYPESCRIPT/3.Looping/LoopingArray/map.ts
@@ -0,0 +1,15 @@
+// THURSDAY, 5 MARCH 2026, 22:05 - 22:35
+
+const name = ["budi", "george", "alexander", "steven"];
+const toUppercaseName = name.map(function (variable, index, array) {
+    return variable.toLocaleUpperCase()
+    // console.log(`${index} : ${variable} : ${array}`)
+})
+console.log(toUppercaseName);
+console.log(name);
+
+const number = [1, 3, 4, 5, 16]
+const multipleNumber = number.map((number)=> {
+    return number * 2
+})
+console.log(multipleNumber)
