bef706466c6672a4ec73096adb66f2e51145875c
Beny0895 <gunadibeny@gmail.com>
2026-03-13 13:21:12 +0700

    Post

--- FRONT-END/FETCH/post.ts
+++ FRONT-END/FETCH/post.ts
@@ -2,28 +2,30 @@
 
 import axios from "axios";
 interface IUser {
-    firstname: string,
-    lastname: string,
-    email: string,
-    password: string
-};
-async function postUser (params: IUser) {
-    try{
-        const response = await axios.post("https://694ba34a26e870772068a7b1.mockapi.io/api/v1/users",
-            {
-                firstname: params.firstname,
-                lastname: params.lastname,
-                email: params.email,
-                password: params.password
-            }
-        );
-        console.log(response.data);
-    } catch (err) {
-        console.log(err) };
-    };
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
+async function postUser(params: IUser) {
+  try {
+    const response = await axios.post(
+      "https://694ba34a26e870772068a7b1.mockapi.io/api/v1/users",
+      {
+        firstname: params.firstname,
+        lastname: params.lastname,
+        email: params.email,
+        password: params.password,
+      },
+    );
+    console.log(response.data);
+  } catch (err) {
+    console.log(err);
+  }
+}
 postUser({
-    firstname: "Budi",
+  firstname: "Budi",
   lastname: "Taniagus",
   email: "budi@mail.com",
-  password: "123456"
+  password: "123456",
 });
