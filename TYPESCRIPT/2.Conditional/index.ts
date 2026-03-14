2b3b309a3dbec8d9750eb7458a17f24b44c85662
Beny0895 <gunadibeny@gmail.com>
2026-03-14 23:38:28 +0700

    Conditional-if/else-SwitchCase

--- TYPESCRIPT/2.Conditional/index.ts
+++ TYPESCRIPT/2.Conditional/index.ts
@@ -3,72 +3,72 @@
 // no 1
 let number1: number = 1;
 if (number1 > 0) {
-    const action = "Positive Number";
-    console.log(action);
+  const action = "Positive Number";
+  console.log(action);
 }
 
 // no 2
 let c: number = 75;
 if (c >= 75) {
-    const result = "passed";
-    console.log(result);
-};
+  const result = "passed";
+  console.log(result);
+}
 
 // no 3
 let isLoggedIn = true;
 if (isLoggedIn) {
-    const action = "User Already Logged In";
-    console.log(action);
+  const action = "User Already Logged In";
+  console.log(action);
 } else {
-    const action = "User Not Logged In";
-    console.log(action);
+  const action = "User Not Logged In";
+  console.log(action);
 }
 
 // no 4
 let number2: number = 1;
 if (number2 % 2 == 0) {
-    const action = "even";
-    console.log(action);
+  const action = "even";
+  console.log(action);
 } else if (number2 % 2 == 1) {
-    const action = "odd";
-    console.log(action);
+  const action = "odd";
+  console.log(action);
 } else {
-    const action = "please enter input";
-    console.log(action);
+  const action = "please enter input";
+  console.log(action);
 }
 
 // no 5
 let d: number = 75;
 if (d >= 75) {
-    const result = "passed";
-    console.log(result);
+  const result = "passed";
+  console.log(result);
 } else {
-    const result = "not passed";
-    console.log(result);
-};
+  const result = "not passed";
+  console.log(result);
+}
 
 // no 6
 let age1: number = 0;
 if (age1 >= 18) {
-    const action = "Allowed to Make Driver’s License";
-    console.log(action);
+  const action = "Allowed to Make Driver’s License";
+  console.log(action);
 } else if (age1 < 18 && age1 > 1) {
-    const action = "Not Allowed to Make Driver’s License";
-    console.log(action);
+  const action = "Not Allowed to Make Driver’s License";
+  console.log(action);
 } else {
-    const action = "Not Born Yet";
-    console.log(action);
+  const action = "Not Born Yet";
+  console.log(action);
 }
 
 // SWITCH CASE
 // no 7
 let day: number = 1;
 switch (day) {
-    case 1:
-        console.log("monday");
-        break;
-    case 2:
-        console.log("tuesday");
-        break;
-    case 3:
-};
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+}
