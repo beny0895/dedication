86b3f79070c7d4323c55158d91141dc89c15547e
Beny0895 <gunadibeny@gmail.com>
2026-03-14 23:40:40 +0700

    Looping-for-while-doWhile

--- TYPESCRIPT/3.Looping/index.ts
+++ TYPESCRIPT/3.Looping/index.ts
@@ -3,65 +3,65 @@
 // no 1
 // Create a program using for to display numbers 1 to 5 in the console.
 for (let a: number = 0; a <= 5; a++) {
-    console.log(a);
-};
+  console.log(a);
+}
 
 // no 2
 // Use for to display even numbers from 2 to 10.
 for (let b: number = 0; b < 10; b += 2) {
-    console.log(b);
-};
+  console.log(b);
+}
 
 // no 3
 // Create a for loop that displays text.
 for (let c: number = 0; c <= 3; c++) {
-    console.log("learn javascript");
-};
+  console.log("learn javascript");
+}
 
 // no 4
 // Use while to display numbers 1 to 5.
 let d: number = 0;
 while (d <= 5) {
-    console.log(d);
-    d++;
-};
+  console.log(d);
+  d++;
+}
 
 // no 5
 // Create a while program that displays numbers 5 to 1 (backward).
 let e: number = 5;
 while (e >= 1) {
-    console.log(e);
-    e--;
-};
+  console.log(e);
+  e--;
+}
 
 // no 6
 // Use while to display odd numbers from 1 to 9.
 let f: number = 9;
 while (f > 0) {
-    console.log(f);
-    f -= 2;
-};
+  console.log(f);
+  f -= 2;
+}
 
 // no 7
 // Create a do...while program that displays numbers 1 to 5.
 let g: number = 0;
 do {
-    g++;
-    console.log(g);
+  g++;
+  console.log(g);
 } while (g <= 5);
 
 // no 8
 // Use do...while to display the text "Hello World" three times.
 let h: number = 0;
 do {
-    h++;
-    console.log("Hello World");
+  h++;
+  console.log("Hello World");
 } while (h <= 3);
 
 // no 9
 // Create a do...while to display numbers 10, 8, 6, 4, 2.
 let i: number = 10;
 do {
-    console.log(i);
-    i -= 2;
+  console.log(i);
+  i -= 2;
 } while (i >= 2);
